5675476e1855dc1524d47e97febb8849134fea8c
Steko98 <150350959+Steko98@users.noreply.github.com>
2024-09-03 18:20:47 +0200

    Add files via upload

---
+++ EJjava.js
@@ -0,0 +1,18 @@
+function getRandomNumbers(range, count) {
+    const numbers = [];
+    while (numbers.length < count) {
+        const randomNumber = Math.floor(Math.random() * range) + 1;
+        if (!numbers.includes(randomNumber)) {
+            numbers.push(randomNumber);
+        }
+    }
+    return numbers.sort((a, b) => a - b);
+}
+
+document.getElementById('generateButton').addEventListener('click', () => {
+    const randomNumbers1to50 = getRandomNumbers(50, 7);
+    const randomNumbers1to12 = getRandomNumbers(12, 2);
+
+    document.getElementById('numbers1to50').textContent = `Prvih 5 brojeva: ${randomNumbers1to50.join(', ')}`;
+    document.getElementById('numbers1to12').textContent = `Druga 2 broja: ${randomNumbers1to12.join(', ')}`;
+});
